7bae8e6821e80c9e18947a4925bb2290a8ea05b9
LeKono <andrej.konotopez@gmail.com>
2018-07-04 19:59:57 +0200

    Setup a som module and SOM implementation

---
+++ som.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class SOM:
+    def __init__(self):
+        """A basic implementation of a Self-Organized-Map.
+        It is inspired by: blog.yhat.com/posts/self-organizing-maps-2.html
+        """
+        pass
